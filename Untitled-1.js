c177a721d0c5e035f334927cfcb5dc31bfa3be89
Imran Ahmed <62918030+ImranAhmed26@users.noreply.github.com>
2020-06-14 16:41:48 +0600

    added newly for learning reach.js

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+let css =document.querySelector("h2");
+let color1 = document.querySelector("#Color1");
+let color2 = document.querySelector("#Color2");
+let body = document.querySelector("#Body");
+
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+function setGradient(){
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value + "," + color2.value + ")";
+    
+    css.textContent = body.style.background + ";";
+}
+
+
+
+
